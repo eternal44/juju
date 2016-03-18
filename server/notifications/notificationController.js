a3686428b62e42599e06402961af926979b677d4
James Youn <jamesyoun710@gmail.com>
2016-03-17 22:50:27 -0700

    (refactor) remove unused dependencies

--- server/notifications/notificationController.js
+++ server/notifications/notificationController.js
@@ -1,5 +1,3 @@
-//var express = require('express');
-//var router = express.Router();
 var pg = require('pg');
 var config = require('./../../config.js');
 var connectionString; // = process.env.DATABASE_URL || config.connectionString;
